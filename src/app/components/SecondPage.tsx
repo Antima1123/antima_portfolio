91f724d6c3cc6cf272ff19bc3d371ad9f8265ab0
antima1123 <antimasinghthakur4@gmail.com>
2024-09-08 21:29:11 +0530

    as

--- src/app/components/SecondPage.tsx
+++ src/app/components/SecondPage.tsx
@@ -2,6 +2,7 @@ import Image from "next/image"
 import Card from "./Card"
 
 const SecondPage = () =>{
+    const as = `'`;
     return(
         <div className="bg-[#2b2d33]  flex flex-col px-[4%] lg:px-[14%] pt-40">
             <div className="lg:flex lg:flex-row md:flex-col  justify-between">
@@ -32,10 +33,10 @@ const SecondPage = () =>{
 
                 <div className="lg:w-1/2 w-full lg:text-start text-center items-center lg:items-start flex flex-col gap-8">
                    <div className="flex flex-col gap-2 w-full lg:w-[85%] font-[600] ">
-                        <div className="text-[20px] text-[#f75023]">I'm a Designer</div>
+                        <div className="text-[20px] text-[#f75023]">I{as}m a Designer</div>
                         <div className="text-[36px]  ">I Can Design Anything You Want</div>
                    </div>
-                    <div className="text-[#b9b5c9] text-[16px] w-full lg:w-[90%]">Hello there! I'm a web designer, and I'm very passionate and dedicated to my work. With 20 years experience as a professional web developer, I have acquired the skills and knowledge necessary to make your project a success. I enjoy every step of the design process, from discussion and collaboration.</div>
+                    <div className="text-[#b9b5c9] text-[16px] w-full lg:w-[90%]">Hello there! I{as}m a web designer, and I{as}m very passionate and dedicated to my work. With 20 years experience as a professional web developer, I have acquired the skills and knowledge necessary to make your project a success. I enjoy every step of the design process, from discussion and collaboration.</div>
                     <button className="w-[8rem]  mt-2 rounded-3xl p-3 bg-[#f75023] hover:bg-transparent border-2 border-red-500 transition-all duration-500">Hire me</button>
                 </div>
             </div>
