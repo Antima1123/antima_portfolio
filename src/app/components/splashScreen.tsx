31579f9d02dabedd50c45fc51758ede09e3b3f0c
antima1123 <antimasinghthakur4@gmail.com>
2024-09-09 18:04:24 +0530

    z 100

--- src/app/components/splashScreen.tsx
+++ src/app/components/splashScreen.tsx
@@ -15,8 +15,8 @@ const SplashScreen: React.FC= () => {
         </motion.div>
 
         {/* the grow covers */}
-        <motion.div className="w-[1px] h-1/2 bg-white absolute" animate={{y: [0,-200,-600]}} transition={{ ease: "easeOut", duration: 1 }}></motion.div>
-        <motion.div className="w-[1px] h-1/2 bg-white absolute bottom-0" animate={{y: [0,200,600]}} transition={{ ease: "easeOut", duration: 1 }}></motion.div>
+        <motion.div className="w-[1px] h-1/2 bg-white absolute z-[100]" animate={{y: [0,-200,-600]}} transition={{ ease: "easeOut", duration: 1 }}></motion.div>
+        <motion.div className="w-[1px] h-1/2 bg-white absolute bottom-0 z-[100]" animate={{y: [0,200,600]}} transition={{ ease: "easeOut", duration: 1 }}></motion.div>
 
 
         {/* screen right side slider */}
