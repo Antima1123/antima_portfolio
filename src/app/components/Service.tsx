85ae988ae935dad9184ebb8c19214ca7710cf4a7
antima1123 <antimasinghthakur4@gmail.com>
2024-09-10 21:14:41 +0530

    update

--- src/app/components/Service.tsx
+++ src/app/components/Service.tsx
@@ -1,13 +1,42 @@
 import ServCard from "./Service_card";
 
 const Service = () =>{
+    const Service =[
+        {
+            title: "Creative Design",
+            price: 99,
+            des: "test",
+            icon: "first",
+            img: "/serv1.png"
+        },
+        {
+            title: "Creative Design",
+            price: 89,
+            des: "test",
+            icon: "second",
+            img: "/serv2.png"
+        },
+        {
+            title: "Creative Design",
+            price: 67,
+            des: "test",
+            icon: "third",
+            img: "/serv3.png"
+        },
+        {
+            title: "Creative Design",
+            price: 50,
+            des: "test",
+            icon: "forth",
+            img: "/serv4.png"
+        }
+    ]
     return(
         <section id="service">
             <div className="lg:flex-row flex-col grid grid-cols-2  gap-6 h-[50rem] pt-32 px-[6%] lg:px-[14%] bg-[#2b2d33] text-white">
-                <ServCard/>
-                <ServCard/>
-                <ServCard/>
-                <ServCard/>
+                {Service.map((link)=>(
+                    <ServCard title={link.title} price={link.price} des={link.des} icon={link.icon} img={link.img}/>
+                ))}
             </div>
         </section>
     )
--- src/app/components/Service_card.tsx
+++ src/app/components/Service_card.tsx
@@ -1,23 +1,43 @@
 import Image from "next/image";
+import { title } from "process";
 import { FaReact } from "react-icons/fa";
+import { BiSolidUserDetail } from "react-icons/bi";
+import { AiFillProduct } from "react-icons/ai";
+import { CgWebsite } from "react-icons/cg";
 
-const ServCard = () =>{
+
+import { string } from "zod";
+
+interface Service{
+    title: string,
+    price: number,
+    des: string ,
+    icon: string,
+    img: string
+}
+const ServCard: React.FC<Service> = ({title, price,des, icon, img}) =>{
     return(
         <div className="bg-[#3f4453] lg:w-[33rem] w-full font-sans h-[23rem] rounded-2xl p-16 flex ">
-           <div className="w-full h-full flex gap-8 ">
-                <div className="w-32 h-32 justify-center flex bg-red-200 rounded-full items-center">
-                    <FaReact size={50} color="red"/>
-                    </div>
-
-                    <div className="w-2/3 gap-2 flex flex-col">
-                        <div className=" text-[28px] font-[600]" >Creative Design </div>
-                        <div className="flex gap-2 items-center">
-                            <p className="text-[18px] font-[600]">Starts from</p>
-                            <p className="text-[20px] font-[600] text-[#f75023]">$99</p>
-                        </div>
-                        <div className="text-[18px] font-[600] text-[#b9b5c9]">web design refers to the design of website that are displayed on the internet. It usually refers to the user experience aspects of website development</div>
-                    </div>
+           <div className="w-full h-full flex justify-between">
+                <div className="w-32 h-32 justify-center flex rounded-full items-center relative">
+                    <Image src={img} alt="" height={100} width={100}/>
+                    { icon === "first" ?<FaReact size={50} color="black" className="absolute"/>
+                    : icon === "second" ? <BiSolidUserDetail size={50} color="black" className="absolute"/>
+                    : icon === "third" ? <AiFillProduct size={50} color="black" className="absolute"/>
+                    : icon === "forth" ? <CgWebsite size={50} color="black" className="absolute"/>
+                    : ""
+                    }
                 </div>
+
+                <div className="w-2/3 gap-2 flex flex-col pl-4">
+                    <div className=" text-[28px] font-[600]" >{title}</div>
+                    <div className="flex gap-2 items-center">
+                        <p className="text-[18px] font-[600]">Starts from</p>
+                        <p className="text-[20px] font-[600] text-[#f75023]">${price}</p>
+                    </div>
+                    <div className="text-[18px] font-[600] text-[#b9b5c9]">{des}</div>
+                </div>
+            </div>
            </div>
     )
 }
