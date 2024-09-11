7bd08787fd6db1bf85705f01192677d9f29e15c1
antima1123 <antimasinghthakur4@gmail.com>
2024-09-11 16:38:28 +0530

    fix

--- src/app/components/Service_big_card.tsx
+++ src/app/components/Service_big_card.tsx
@@ -12,8 +12,8 @@ const ServiceCard =()=>{
                     <p>Starts from $99</p>
                     <h6>
                         Dizme is a leading web design agency with an award-winning design team that creates innovative, effective websites that capture your brand, improve your conversion rates, and maximize your revenue to help grow your business and achieve your goals.
-                        In today’s digital world, your website is the first interaction consumers have with your business. That's why almost 95 percent of a user’s first impression relates to web design. It’s also why web design services can have an immense impact on your company’s bottom line.
-                        That’s why more companies are not only reevaluating their website’s design but also partnering with Kura, the web design agency that’s driven more than $2.4 billion in revenue for its clients. With over 50 web design awards under our belt, we're confident we can design a custom website that drives sales for your unique business.
+                        In today&apos;s digital world, your website is the first interaction consumers have with your business. That&aposs why almost 95 percent of a user&apos;s first impression relates to web design. It&apos;s also why web design services can have an immense impact on your company&apos;s bottom line.
+                        That&apos;s why more companies are not only reevaluating their website&apos;s design but also partnering with Kura, the web design agency that&apos;s driven more than $2.4 billion in revenue for its clients. With over 50 web design awards under our belt, we&aposre confident we can design a custom website that drives sales for your unique business.
                     </h6>
                 </div>
             </div>
@@ -21,3 +21,5 @@ const ServiceCard =()=>{
     )
 }
 export default ServiceCard
+
+// ’ ye different hai ' isse
